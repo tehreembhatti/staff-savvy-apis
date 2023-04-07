6ab6dc933d661bb9321f5589f9e3bd798edd1b0e
sedhha <activity.schoolsh2@gmail.com>
2023-04-07 08:56:10 +0530

    removed unused imports

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import { App } from '@/app';
-import { AuthRoute } from '@routes/auth.route';
-import { UserRoute } from '@routes/users.route';
 import { ValidateEnv } from '@utils/validateEnv';
 
 ValidateEnv();
 
-const app = new App([new UserRoute(), new AuthRoute()]);
+const app = new App([]);
 
 app.listen();
