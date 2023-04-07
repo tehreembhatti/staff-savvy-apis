7934e3dcac75b126703c4c1114fab6f7e74a7c35
sedhha <activity.schoolsh2@gmail.com>
2023-04-07 08:23:55 +0530

    Trying to add comment

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { AuthRoute } from '@routes/auth.route';
 import { UserRoute } from '@routes/users.route';
 import { ValidateEnv } from '@utils/validateEnv';
 
+// Env
 ValidateEnv();
 
 const app = new App([new UserRoute(), new AuthRoute()]);
